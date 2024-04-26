95610e329582ebe039c4c511a2bd654d26c6a6d2
Furkan Yücel <47148709+furkanyucel@users.noreply.github.com>
2024-04-26 22:28:43 +0300

    Image Updated

--- src/Data/ProductData.js
+++ src/Data/ProductData.js
@@ -6,28 +6,28 @@ export const products = [
     name: "Bambu Diş Fırçası", 
     category: "Kişisel Bakım",
     content: "Yüksek kaliteli paslanmaz çelikten üretilen bu su şişesi, günlük su tüketiminize sofistike bir dokunuş katmak için mat bir yüzeye sahiptir.",
-    imgUrl : "/public/BambuDisFircasi.webp",
+    imgUrl : "/public/BambuDisFircasi.png",
    },
   { 
     id: 2, 
     name: "Yeniden Kullanılabilir Su Şişesi", 
     category: "Mutfak",
     content: "Yüksek kaliteli paslanmaz çelikten üretilen bu su şişesi, günlük su tüketiminize sofistike bir dokunuş katmak için mat bir yüzeye sahiptir.",
-    imgUrl : "/public/YenidenKullanilabilirSuSisesi.webp",
+    imgUrl : "/public/YenidenKullanilabilirSuSisesi.png",
    },
   { 
     id: 3, 
     name: "Organik Pamuklu Tişört", 
     category: "Giyim",
     content: "Yüksek kaliteli paslanmaz çelikten üretilen bu su şişesi, günlük su tüketiminize sofistike bir dokunuş katmak için mat bir yüzeye sahiptir.",
-    imgUrl : "/public/OrganikPamukluTShirt.webp",
+    imgUrl : "/public/OrganikPamukluTShirt.png",
    },
   { 
     id: 4, 
     name: "Organik Keten Pantalon", 
     category: "Giyim",
     content: "Yüksek kaliteli paslanmaz çelikten üretilen bu su şişesi, günlük su tüketiminize sofistike bir dokunuş katmak için mat bir yüzeye sahiptir.",
-    imgUrl : "/public/OrganikKetenPantalon.webp",
+    imgUrl : "/public/OrganikKetenPantalon.png",
    }
 
 ]
