8242ef25cc548ac54704db8c84f321d4a5d28c17
furkanyucel <furkanyucelux@gmail.com>
2024-04-26 21:37:03 +0300

    Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from "./Home";
-import ProductList from "./components/Product/ProductList";
-import ProductDetails from "./components/Product/ProductDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+// pages
+import Home from "./pages/Home";
+import Header from "./pages/Header";
+import ProductList from "./components/Product/ProductList";
+// import ProductDetails from "./components/Product/ProductDetails";
+
+// css
+// import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/products" exact component={ProductList} />
-        <Route path="/products/:productId" component={ProductDetails} />
-      </Switch>
-    </Router>
+
+<Router>
+
+  <Header />
+
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/product" element={<ProductList />} />
+  </Routes>
+  
+</Router>
   );
 }
 
