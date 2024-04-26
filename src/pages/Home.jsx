95e11bee50258c6798ab8bb30eebaffb75ae173b
furkanyucel <furkanyucelux@gmail.com>
2024-04-26 21:35:38 +0300

    Homepage Created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-[750px] ">
+      <h1 className='text-5xl font-semibold'>Eco Store'a Hoş Geldiniz!</h1>
+      <p className='text-3xl font-medium my-8'>Çevre dostu ürünlerimizi keşfetmek için doğru yerdesiniz.</p>
+      <p className='text-xl font-base'>Burada, sürdürülebilirlik ve doğa dostu malzemelerle üretilmiş ürünleri bulabilirsiniz.</p>
+    </div>
+  );
+}
+
+export default Home;
