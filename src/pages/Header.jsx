20a4fa5732fe407dd45b51582165f7c8cef1e0f1
furkanyucel <furkanyucelux@gmail.com>
2024-04-27 19:46:35 +0300

    Search Bar Added and Updated

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,22 +1,24 @@
 import { NavLink } from "react-router-dom";
+import SearchBar from "../UI/SearchBar/SearchBar";
 
 const Header = () => {
   return (
-    <nav className="bg-green-700 w-full p-3 ">
-    <ul className="flex gap-4 font-semibold text-xl text-slate-50 :text-orange-500">
-      <li>
-        <NavLink to="/" end style={({ isActive }) => ({ color: isActive ? 'white' : 'rgb(20 83 45)' })}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/product" style={({ isActive }) => ({ color: isActive ? 'white' : 'rgb(20 83 45)' })}>
-          Product
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-  )
+    <nav className="bg-green-700 w-full px-3 flex justify-between items-center">
+      <ul className="flex gap-4 font-semibold text-xl text-slate-50">
+        <li>
+          <NavLink to="/" end style={({ isActive }) => ({ color: isActive ? 'white' : 'rgb(20 83 45)' })}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/product" style={({ isActive }) => ({ color: isActive ? 'white' : 'rgb(20 83 45)' })}>
+            Product
+          </NavLink>
+        </li>
+      </ul>
+      <SearchBar />
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
