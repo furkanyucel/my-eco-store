3c87d880a4686d8c2704105adcd2faa41cae6346
furkanyucel <furkanyucelux@gmail.com>
2024-04-27 19:45:40 +0300

    Updated

--- src/UI/Buttons/Button.jsx
+++ src/UI/Buttons/Button.jsx
@@ -1,11 +1,11 @@
+import React from 'react';
 
-
-const Button = ({children, onClick}) => {
+const Button = ({ children, onClick }) => {
   return (
     <button onClick={onClick} className='bg-green-500 hover:bg-green-600 choosed:bg-orange-500 hover:border-transparent text-white py-2 px-4 rounded mx-1 my-6'>
       {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
