50657fc5399a8045eae4c24fc8114b161998415d
Furkan Yücel <47148709+furkanyucel@users.noreply.github.com>
2024-04-27 04:03:24 +0300

    Image Link Test

--- src/Data/ProductData.js
+++ src/Data/ProductData.js
@@ -6,7 +6,7 @@ export const products = [
     name: "Bambu Diş Fırçası", 
     category: "Kişisel Bakım",
     content: "Yüksek kaliteli paslanmaz çelikten üretilen bu su şişesi, günlük su tüketiminize sofistike bir dokunuş katmak için mat bir yüzeye sahiptir.",
-    imgUrl : "/public/BambuDisFircasi.png",
+    imgUrl : "https://files.oaiusercontent.com/file-j1cTAN7vF8uQ5OpKdFoYcAxh?se=2024-04-27T01%3A06%3A25Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dca323f39-8bc6-472c-805c-7637bf9752fa.webp&sig=DbZ/lP9IvK1exEWdDgEnrjA/ryNtFFGph8IoIOvzrV0%3D",
    },
   { 
     id: 2, 
